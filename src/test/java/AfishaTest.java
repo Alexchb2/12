eaa4306cfb9c62075aa35f5cb408c06574be9a42
alexc <alexchb.makarenko@yandex.ru>
2022-09-01 10:25:52 +0300

    first

---
+++ src/test/java/AfishaTest.java
@@ -0,0 +1,90 @@
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+public class AfishaTest {
+
+    @Test
+    public void Movies() {
+        Afisha afisha = new Afisha();
+        afisha.add("Movie 1");
+        afisha.add("Movie 2");
+        afisha.add("Movie 3");
+        afisha.add("Movie 4");
+        afisha.add("Movie 5");
+        afisha.add("Movie 6");
+        afisha.add("Movie 7");
+        afisha.add("Movie 8");
+        afisha.add("Movie 9");
+        afisha.add("Movie 10");
+        afisha.add("Movie 11");
+        afisha.add("Movie 12");
+        String[] expected = {"Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5", "Movie 6", "Movie 7", "Movie 8", "Movie 9", "Movie 10", "Movie 11", "Movie 12"};
+        String[] actual = afisha.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveMovies() {
+        Afisha afisha = new Afisha(12);
+        afisha.add("Movie 1");
+        afisha.add("Movie 2");
+        afisha.add("Movie 3");
+        afisha.add("Movie 4");
+        afisha.add("Movie 5");
+        afisha.add("Movie 6");
+        afisha.add("Movie 7");
+        afisha.add("Movie 8");
+        afisha.add("Movie 9");
+        afisha.add("Movie 10");
+        afisha.add("Movie 11");
+        afisha.add("Movie 12");
+        String[] expected = {"Movie 12", "Movie 11", "Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = afisha.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveMovies5() {
+        Afisha afisha = new Afisha(5);
+        afisha.add("Movie 1");
+        afisha.add("Movie 2");
+        afisha.add("Movie 3");
+        afisha.add("Movie 4");
+        afisha.add("Movie 5");
+        afisha.add("Movie 6");
+        afisha.add("Movie 7");
+        afisha.add("Movie 8");
+        afisha.add("Movie 9");
+        afisha.add("Movie 10");
+        afisha.add("Movie 11");
+        afisha.add("Movie 12");
+        String[] expected = {"Movie 12", "Movie 11", "Movie 10", "Movie 9", "Movie 8"};
+        String[] actual = afisha.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveMovies13() {
+        Afisha afisha = new Afisha(13);
+        afisha.add("Movie 1");
+        afisha.add("Movie 2");
+        afisha.add("Movie 3");
+        afisha.add("Movie 4");
+        afisha.add("Movie 5");
+        afisha.add("Movie 6");
+        afisha.add("Movie 7");
+        afisha.add("Movie 8");
+        afisha.add("Movie 9");
+        afisha.add("Movie 10");
+        afisha.add("Movie 11");
+        afisha.add("Movie 12");
+        String[] expected = {"Movie 12", "Movie 11", "Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = afisha.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
